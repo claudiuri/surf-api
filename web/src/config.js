e3f84a198b2b9aad08b791925f66ee3a88116d73
claudiuri <claudio.y07@gmail.com>
2021-03-11 18:48:17 -0300

    feat: update API_URL

--- web/src/config.js
+++ web/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL ?? 'http://localhost:3000';
-export const GMAPS_API_KEY = process.env.GMAPS_API_KEY ?? 'AIzaSyBB854Viw6N2qgak_TT-C91kHKky82POYw';
+export const API_URL = process.env.API_URL;
+export const GMAPS_API_KEY = process.env.GMAPS_API_KEY;
