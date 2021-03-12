4f255510923503a09ec4ec0b7241f93ac350e487
claudiuri <claudio.y07@gmail.com>
2021-03-12 08:59:13 -0300

    feat: update enviroment variable

--- web/src/config.js
+++ web/src/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const API_URL = process.env.API_URL;
-export const GMAPS_API_KEY = process.env.GMAPS_API_KEY;
+export const API_URL = process.env.REACT_APP_API_URL;
+export const GMAPS_API_KEY = process.env.REACT_APP_GMAPS_API_KEY;
